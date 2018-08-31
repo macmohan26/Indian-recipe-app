af5fe521369ccf45c4097c4dc16ad91340bbe931
mrathore <mrathore@unomaha.edu>
2018-08-31 15:09:00 -0500

    first commit

---
+++ src/main/java/sonu/springframework/indianrecipeapp/IndianRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package sonu.springframework.indianrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndianRecipeAppApplication.class, args);
+    }
+}
---
+++ src/test/java/sonu/springframework/indianrecipeapp/IndianRecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package sonu.springframework.indianrecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndianRecipeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
